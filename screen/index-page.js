4ce583cb32b83deb7c41c29fbcbcf266d4d37f98
Esteban Gonzalez <ldarcknes@hotmail.com>
2022-06-07 14:49:54 -0500

    Add introduction

--- screen/index-page.js
+++ screen/index-page.js
@@ -3,11 +3,13 @@ import HomeScreen from "./Home";
 import LoginScreen from "./Login";
 import ProfileScreen from "./Profile";
 import SupportScreen from "./Support";
+import IntroductionScreen from "./Introduction";
 
 export {
           HomeScreen as Home,
           CalendarScreen as Calendar,
           LoginScreen as Login,
           ProfileScreen as Profile,
-          SupportScreen as Support
+          SupportScreen as Support,
+          IntroductionScreen as Introduction,
         };
