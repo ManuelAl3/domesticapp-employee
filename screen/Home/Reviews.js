f1f11633479af487e4ecc0b30c161355c4b12ab0
Esteban Gonzalez <ldarcknes@hotmail.com>
2022-06-07 15:31:30 -0500

    Add home routes

---
+++ screen/Home/Reviews.js
@@ -0,0 +1,12 @@
+import { StyleSheet, Text, View, Button } from "react-native";
+import * as React from "react";
+
+function ReviewsScreen({ navigation }) {
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>Reviews Screen</Text>
+    </View>
+  );
+}
+
+export default ReviewsScreen;
