324f8bd4cf618ec406f67729193f658af196e70a
Esteban Gonzalez <ldarcknes@hotmail.com>
2022-06-07 14:40:14 -0500

    Add index import

---
+++ screen/index-page.js
@@ -0,0 +1,13 @@
+import CalendarScreen from "./Calendar";
+import HomeScreen from "./Home";
+import LoginScreen from "./Login";
+import ProfileScreen from "./Profile";
+import SupportScreen from "./Support";
+
+export {
+          HomeScreen as Home,
+          CalendarScreen as Calendar,
+          LoginScreen as Login,
+          ProfileScreen as Profile,
+          SupportScreen as Support
+        };
