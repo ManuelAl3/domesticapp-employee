f6500486c00fea272bdcd49d4646bc2109089809
Esteban Gonzalez <ldarcknes@hotmail.com>
2022-06-07 14:39:53 -0500

    Add Screens

---
+++ screen/Support.js
@@ -0,0 +1,12 @@
+import { StyleSheet, Text, View, Button } from "react-native";
+import * as React from "react";
+
+function SupportScreen({ navigation }) {
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>Support Screen</Text>
+    </View>
+  );
+}
+
+export default SupportScreen;
