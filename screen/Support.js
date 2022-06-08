84d0d5aca8206210861447de52b3a22c1bd1b715
Esteban Gonzalez <ldarcknes@hotmail.com>
2022-06-08 17:00:11 -0500

    Add support screen

--- screen/Support.js
+++ screen/Support.js
@@ -1,11 +1,22 @@
-import { StyleSheet, Text, View, Button } from "react-native";
+import { StyleSheet, Text, View, Image, ScrollView } from "react-native";
 import * as React from "react";
+import { supportOptions } from "../constants/supportO";
 
 function SupportScreen({ navigation }) {
   return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <Text>Support Screen</Text>
-    </View>
+    <ScrollView>
+      {
+        supportOptions.map((option)=>(
+          <View key={option.id}>
+          <Image 
+            style={{width: 100, height: 100}}
+            source={option.img}
+          />
+          <Text onPress={() => navigation.navigate(`${option.ruta}`,{ itemId: option.id})}>{option.nombre}</Text>
+          </View>
+        ))
+      }
+    </ScrollView>
   );
 }
 
