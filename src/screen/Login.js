ae8320687f550ebf5d404d52f2c9f0ff6ff3247d
Esteban Gonzalez <ldarcknes@hotmail.com>
2022-06-09 19:36:56 -0500

    Add Profile and my profile screens

--- src/screen/Login.js
+++ src/screen/Login.js
@@ -7,7 +7,7 @@ function LoginScreen({ navigation }) {
   return (
     <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
       <Text>Login Screen</Text>
-      <Button onPress={()=>login({email: "customer@mail.com", password: "123456"})}>Login</Button>
+      <Button onPress={()=>login({email: "employee@mail.com", password: "123456"})}>Login</Button>
     </View>
   );
 }
---
+++ src/screen/Profile/My-profile.js
@@ -0,0 +1,51 @@
+import { StyleSheet, Text, View, Image, Button } from "react-native";
+import * as React from "react";
+import { useAuth } from "../../context/auth-context";
+import { showHEmployee } from "../../services/hability-employee-services";
+
+function MyProfileScreen({ navigation }) {
+  const { user } = useAuth();
+  const [ skills, setSkills] = React.useState(null);
+  React.useEffect(() => {
+    showHEmployee(user.id).then(setSkills);
+  }, [user.id])
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Image 
+        style={styles.tinyLogo}
+        source={user.image_url}
+      />
+      <Text>{user.full_name}</Text>
+      {
+        skills ? (
+          skills.map((skills) =>(
+            <View key={skills.id}>
+              <Text>{skills.hability}</Text>
+            </View>
+          ))
+          ) : (<Text>Sin habilidades</Text>)
+      }
+      <Text>Mi biogragia</Text>
+
+
+      <Text>Mis Datos Personales</Text>
+      <Text>Nacionalidad {user.country}</Text>
+      <Text>Teléfono {user.phone}</Text>
+      <Text>Email {user.email}</Text>
+      <Text>Edad</Text>
+      <Text>CC {user.document_id}</Text>
+
+      <Button title="Ver Reseñas de Clientes" />
+    </View>
+  );
+}
+const styles = StyleSheet.create({
+  container: {
+    paddingTop: 50,
+  },
+  tinyLogo: {
+    width: 50,
+    height: 50,
+  },});
+
+export default MyProfileScreen;
