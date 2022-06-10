3862ef86441bb1610f626b531d5c8c624d689ec0
Esteban Gonzalez <ldarcknes@hotmail.com>
2022-06-10 18:43:47 -0500

    Add part of component

--- src/screen/Home/Certifications.js
+++ src/screen/Home/Certifications.js
@@ -1,14 +1,16 @@
 import { StyleSheet, Text, View, Button } from "react-native";
 import * as React from "react";
 import BackTitledHeader from "../../components/BackTitleHeader";
+import { useAuth } from "../../context/auth-context";
 
 function CertificationScreen({ navigation }) {
+  const {user}= useAuth();
   return (
     <>
     <BackTitledHeader title="Mi Certificado Laboral" />
     <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
       <Text>Datos de certificado</Text>
-      <Text>Nombre: </Text>
+      <Text>Nombre: {user.full_name}</Text>
       <Text>Inicio de contrato: </Text>
       <Text>Puesto: </Text>
     </View>
