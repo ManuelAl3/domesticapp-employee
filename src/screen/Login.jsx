c2a154f065ddcc2ba32f0af5883798ea5fce199e
Esteban Gonzalez <ldarcknes@hotmail.com>
2022-07-11 15:48:33 -0500

    Add covers app

--- src/screen/Login.jsx
+++ src/screen/Login.jsx
@@ -13,15 +13,18 @@ import { Picker } from "@react-native-picker/picker";
 import { COLORS } from "../../config";
 import { useAuth } from "../context/auth-context";
 import AnimatedInput from "react-native-animated-input";
+import Constants from "expo-constants";
 
 function LoginScreen() {
   const [form, setForm] = useState({
     email: "",
     password: "",
   });
+
+  const version = Constants.manifest.version;
   const { login } = useAuth();
-  const [errors, setErrors] = useState(null)
-  const [isValid, setIsValid] = useState(true)
+  const [errors, setErrors] = useState(null);
+  const [isValid, setIsValid] = useState(true);
   const btnStyle = {
     height: 55,
     width: "100%",
@@ -32,11 +35,10 @@ function LoginScreen() {
     marginBottom: 16,
   };
   function handleSubmit() {
-    
     login(form).catch((error) => {
       setErrors(error.message);
       setIsValid(!isValid);
-    })
+    });
   }
 
   function handleEmailChange(event) {
@@ -47,7 +49,6 @@ function LoginScreen() {
   }
 
   return (
-
     <SafeAreaView
       style={{
         flex: 1,
@@ -56,7 +57,6 @@ function LoginScreen() {
         backgroundColor: COLORS.blue,
       }}
     >
- 
       <View
         style={{
           width: "100%",
@@ -72,38 +72,36 @@ function LoginScreen() {
           backgroundColor: colors.blue,
         }}
       >
-      <AnimatedInput
-        placeholder="Correo"
-        valid={isValid}
-        errorText="Error"
-        onChangeText={handleEmailChange}
-        value={form.email}
-        styleLabel={{ fontWeight: "600"}}
-        styleContent= {{marginBottom: 10,}}
-        styleBodyContent={{ borderBottomWidth: 1.5, marginTop: 10 }}
-      />
-      <AnimatedInput
-        placeholder="Contraseña"
-        valid={isValid}
-        errorText={errors}
-        onChangeText={handlePasswordChange}
-        value={form.password}
-        styleContent= {{marginTop: 10, fontSize: 8}}
-        styleLabel={{ fontWeight: "600" }}
-        styleBodyContent={{ borderBottomWidth: 1.5, fontSize: 8, }}
-      />
-        
-        
-          <View style={styles.containerButton}>
-            <TouchableOpacity style={btnStyle} onPress={handleSubmit}>
-              <Text style={styles.textButton}>Iniciar Sesión</Text>
-            </TouchableOpacity>
-          </View>
-          <Text style={styles.textFooter}>
-            Domesticapp Ver.1.01 2022 Derechos Reservados
-          </Text>
+        <AnimatedInput
+          placeholder="Correo"
+          valid={isValid}
+          errorText="Error"
+          onChangeText={handleEmailChange}
+          value={form.email}
+          styleLabel={{ fontWeight: "600" }}
+          styleContent={{ marginBottom: 10 }}
+          styleBodyContent={{ borderBottomWidth: 1.5, marginTop: 10 }}
+        />
+        <AnimatedInput
+          placeholder="Contraseña"
+          valid={isValid}
+          errorText={errors}
+          onChangeText={handlePasswordChange}
+          value={form.password}
+          styleContent={{ marginTop: 10, fontSize: 8 }}
+          styleLabel={{ fontWeight: "600" }}
+          styleBodyContent={{ borderBottomWidth: 1.5, fontSize: 8 }}
+        />
+
+        <View style={styles.containerButton}>
+          <TouchableOpacity style={btnStyle} onPress={handleSubmit}>
+            <Text style={styles.textButton}>Iniciar Sesión</Text>
+          </TouchableOpacity>
         </View>
-      
+        <Text style={styles.textFooter}>
+          {"Domesticapp Ver." + version + " 2022 Derechos Reservados"}
+        </Text>
+      </View>
     </SafeAreaView>
   );
 }
@@ -112,148 +110,148 @@ export default LoginScreen;
 
 const styles = StyleSheet.create({
   mainContainer: {
-      flex: 1,
-      backgroundColor: COLORS.lightGray,
+    flex: 1,
+    backgroundColor: COLORS.lightGray,
   },
   footerColor: {
-      color: '#222'
+    color: "#222",
   },
   tabScreenContainer: {
-      alignItems: 'center',
+    alignItems: "center",
   },
   centerContainer: {
-      flex: 1,
-      width: '85%',
-      paddingVertical: 15,
-      alignItems: 'center',
+    flex: 1,
+    width: "85%",
+    paddingVertical: 15,
+    alignItems: "center",
   },
   buttonContainer: {
-      width: '100%',
-      alignItems: 'center',
-      position: "absolute",
-      bottom: 10,
+    width: "100%",
+    alignItems: "center",
+    position: "absolute",
+    bottom: 10,
   },
   btnAction: {
-      marginBottom: 15,
-      padding: 10,
-      backgroundColor: COLORS.primary
+    marginBottom: 15,
+    padding: 10,
+    backgroundColor: COLORS.primary,
   },
   btnTextAction: {
-      fontSize: 18,
-      fontFamily: 'Montserrat_600SemiBold',
+    fontSize: 18,
+    fontFamily: "Montserrat_600SemiBold",
   },
   inputsContainer: {
-      width: '100%',
-      justifyContent: 'flex-start'
+    width: "100%",
+    justifyContent: "flex-start",
   },
   header: {
-      backgroundColor: '#fff',
-      borderBottomStartRadius: 30,
-      borderBottomEndRadius: 30,
-      alignItems: 'center',
-      marginBottom: 5,
-      shadowColor: "rgba(0, 0, 0, 0.5)",
-      shadowOffset: {
-          width: 0,
-          height: 1,
-      },
-      shadowOpacity: 0.20,
-      shadowRadius: 1.41,
-      elevation: 2,
+    backgroundColor: "#fff",
+    borderBottomStartRadius: 30,
+    borderBottomEndRadius: 30,
+    alignItems: "center",
+    marginBottom: 5,
+    shadowColor: "rgba(0, 0, 0, 0.5)",
+    shadowOffset: {
+      width: 0,
+      height: 1,
+    },
+    shadowOpacity: 0.2,
+    shadowRadius: 1.41,
+    elevation: 2,
   },
   tabs: {
-      flexDirection: 'row',
-      width: '85%',
-      justifyContent: 'space-between',
+    flexDirection: "row",
+    width: "85%",
+    justifyContent: "space-between",
   },
   tab: {
-      width: '50%'
+    width: "50%",
   },
   tabText: {
-      textAlign: 'center',
-      fontSize: 18,
-      marginBottom: 10,
+    textAlign: "center",
+    fontSize: 18,
+    marginBottom: 10,
   },
   tabIndicator: {
-      backgroundColor: COLORS.secondary,
-      height: 3,
-      width: '100%'
+    backgroundColor: COLORS.secondary,
+    height: 3,
+    width: "100%",
   },
   extraSpace: {
-      height: 100
+    height: 100,
   },
   inputBlueRounded: {
-      borderWidth: 2,
-      borderBottomWidth: 2,
-      borderColor: '#0BBBEF',
-      backgroundColor: '#FFFFFF',
-      borderRadius: 13,
-      height: 45,
-      paddingHorizontal: 15,
-      fontFamily: 'Poppins_500Medium',
-      marginBottom: 5,
+    borderWidth: 2,
+    borderBottomWidth: 2,
+    borderColor: "#0BBBEF",
+    backgroundColor: "#FFFFFF",
+    borderRadius: 13,
+    height: 45,
+    paddingHorizontal: 15,
+    fontFamily: "Poppins_500Medium",
+    marginBottom: 5,
   },
   labelForInput: {
-      fontSize: 17,
-      color: '#fff',
+    fontSize: 17,
+    color: "#fff",
   },
   pressableImage: {
-      marginBottom: 20,
-      height: 144,
-      width: 144,
-      alignSelf: "center",
+    marginBottom: 20,
+    height: 144,
+    width: 144,
+    alignSelf: "center",
   },
   imagePlaceholder: {
-      height: 144,
-      width: 144,
-      backgroundColor: '#D9D9D9',
-      justifyContent: "center",
-      alignItems: "center",
-      borderRadius: 72
+    height: 144,
+    width: 144,
+    backgroundColor: "#D9D9D9",
+    justifyContent: "center",
+    alignItems: "center",
+    borderRadius: 72,
   },
   iconCamera: {
-      fontSize: 42,
-      color: '#838383'
+    fontSize: 42,
+    color: "#838383",
   },
   iconErase: {
-      fontSize: 20,
-      color: '#FFF'
+    fontSize: 20,
+    color: "#FFF",
   },
   textAddImage: {
-      color: '#838383',
-      fontFamily: 'Poppins_500Medium',
-      fontSize: 12
+    color: "#838383",
+    fontFamily: "Poppins_500Medium",
+    fontSize: 12,
   },
   imagePhoto: {
-      height: 144,
-      width: 144,
-      borderRadius: 72
+    height: 144,
+    width: 144,
+    borderRadius: 72,
   },
   buttonErase: {
-      position: "absolute",
-      bottom: 0,
-      right: 0,
-      backgroundColor: '#F00',
-      borderRadius: 15,
-      justifyContent: "center",
-      alignItems: "center",
-      height: 25,
-      width: 25,
+    position: "absolute",
+    bottom: 0,
+    right: 0,
+    backgroundColor: "#F00",
+    borderRadius: 15,
+    justifyContent: "center",
+    alignItems: "center",
+    height: 25,
+    width: 25,
   },
   picker: {
-      borderWidth: 2,
-      borderBottomWidth: 2,
-      borderColor: '#0BBBEF',
-      backgroundColor: '#FFFFFF',
-      borderRadius: 13,
-      height: 45,
-      justifyContent: 'center',
+    borderWidth: 2,
+    borderBottomWidth: 2,
+    borderColor: "#0BBBEF",
+    backgroundColor: "#FFFFFF",
+    borderRadius: 13,
+    height: 45,
+    justifyContent: "center",
   },
   pickerLabel: {
-      opacity: 0.4,
-      marginBottom: 5
+    opacity: 0.4,
+    marginBottom: 5,
   },
   textPicker: {
-      backgroundColor: '#D9D9D9',
-  }
+    backgroundColor: "#D9D9D9",
+  },
 });
